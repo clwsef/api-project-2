fb1cf2c895788c48ecb50331b2b259180bb2a48f
clwsef <135839448+clwsef@users.noreply.github.com>
2023-06-11 21:01:52 +0600

    Создаем функцию

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const jokeContainer=document.querySelector('#joke')
 const btn=document.querySelector('#btn')
 const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+let getJoke=()=>{
+
+}
