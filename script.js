84304655bb3608e962e5f426e8cef160ddb8c52d
clwsef <135839448+clwsef@users.noreply.github.com>
2023-06-11 21:03:38 +0600

    Затухание текста

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const jokeContainer=document.querySelector('#joke')
 const btn=document.querySelector('#btn')
 const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
 let getJoke=()=>{
+    jokeContainer.classList.remove('fade')
 
 }
