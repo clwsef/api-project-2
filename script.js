21239fba0d54ba423987265b76f1f01344a11e6a
clwsef <135839448+clwsef@users.noreply.github.com>
2023-06-11 21:04:00 +0600

    fetch

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const btn=document.querySelector('#btn')
 const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
 let getJoke=()=>{
     jokeContainer.classList.remove('fade')
+    fetch(url)
 
 }
